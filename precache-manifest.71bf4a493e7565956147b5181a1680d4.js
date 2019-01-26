2a7ddacb6e42ce8be32a41feb440b41797f527f5
NIckita <ya.nik34@yandex.ru>
2019-01-26 19:53:23 +0300

    Updates

---
+++ precache-manifest.71bf4a493e7565956147b5181a1680d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "da85ffda05599c3a13ee",
+    "url": "/DemoNotes/static/js/runtime~main.da85ffda.js"
+  },
+  {
+    "revision": "4340a98c1619e2499f51",
+    "url": "/DemoNotes/static/js/main.4340a98c.chunk.js"
+  },
+  {
+    "revision": "fbfa9ac1c04d6f818c50",
+    "url": "/DemoNotes/static/js/1.fbfa9ac1.chunk.js"
+  },
+  {
+    "revision": "4340a98c1619e2499f51",
+    "url": "/DemoNotes/static/css/main.8a140992.chunk.css"
+  },
+  {
+    "revision": "9a90241f5806c3ff2118b58f87b3ec8a",
+    "url": "/DemoNotes/index.html"
+  }
+];
